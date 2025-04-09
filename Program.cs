3f6c8e76a1c4e6f364334e182be5b44c5972b792
nguyentien179 <nguyentien100333@gmail.com>
2025-04-09 22:18:56 +0700

    final

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
 using R2ETien.EFCore.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
